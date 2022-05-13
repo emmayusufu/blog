797ef9c84b53a39510932065146ab7e6a9f2888e
Kimaswa Emmanuel Yusufu <kimaswaemma36@gmail.com>
2022-05-13 14:15:36 +0300

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+;
+
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+    public function index(): View
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        auth()->attempt($request->only('email','password'));
+
+        return redirect()->route('dashboard');
+    }
+}
